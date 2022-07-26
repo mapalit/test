563eec85403ed61dc58f6979f828420718ac218c
mapalit <109716170+mapalit@users.noreply.github.com>
2022-07-26 10:26:38 +0300

    Update mapalit test.js

--- mapalit test.js
+++ mapalit test.js
@@ -6,4 +6,5 @@ console.log("saying goodbye is a death by a thousand cuts");
 console.log("I'll be alright it's just a thousand cuts");
 console.log("saying goodbye is a death by a thousand cuts");
 console.log("I'll be alright it's just a thousand cuts");
-console.log("you who charmed my dad")
+console.log("you who charmed my dad");
+console.log("I loved you so");
